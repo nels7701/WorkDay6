20682c46ed3ea2fda0496a0aac64b2bdc2edaece
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:12:14 -0500

    finished

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,13 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
-
         lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
-
+        Dim n, n2 As Integer
+        n = 1
+        Do While n <= 10
+            n2 = n * n
+            lstResult.Items.Add(CStr(n) & " squared is " & CStr(n2))
+            n = n + 1
+        Loop
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -3,8 +3,16 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
+        Dim _population As Double = 7000000000
+        Dim current As Integer = 2012
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Do While _population > 6000000
+            current = current - 50
+            _population = _population / 2
+
+        Loop
+
+        MessageBox.Show("The earth's population was less than 6 million in approximately " & CStr(current))
 
     End Sub
 End Class
